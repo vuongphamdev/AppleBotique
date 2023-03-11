2b86644c1adb7642e272a7ef2e61cf9a16824d49
vuongpham <vuong.pham@evizi.com>
2023-03-11 11:07:31 +0700

    red test

--- src/components/Icon/BackIcon.test.jsx
+++ src/components/Icon/BackIcon.test.jsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import BackIcon from './BackIcon';
 
 const renderComponent = () => {
-  return render(<BackIcon />);
+  render(<BackIcon />);
 };
 
 it('can render component', () => {
